d7974b20f6ba2d0f24d1527356fc43d5e9ead0ec
Shibata Hironori <46587015+hilosiva@users.noreply.github.com>
2025-01-30 19:06:15 +0900

    update: npm publish

---
+++ tsup.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "tsup";
+import { version } from "./package.json";
+
+export default defineConfig([
+
+  {
+    entry: {
+      oreset: "src/oreset.css",
+    },
+    minify: true,
+    sourcemap: false,
+    clean: true,
+    target: "es2020",
+    outDir: "dist",
+    banner: {
+      css: `/*! Oreset.css v${version} | MIT License | https://github.com/hilosiva/Oreset.css */`,
+    },
+    esbuildOptions: (options) => {
+      options.legalComments = "none";
+    },
+  },
+]);
